c48bfe76a2bcb32527cea34ce4f702e9923ffd76
aadi1094 <adityachawale14@gmail.com>
2026-06-19 13:00:57 +0530

    Arithmetic Operators

---
+++ src/Operators/ArithmeticOperators.java
@@ -0,0 +1,31 @@
+package Operators;
+
+public class ArithmeticOperators {
+    int a = 10;
+    int b=7;
+
+    public static void main(String[] args) {
+
+
+        int x = 10;
+        int y = 3;
+
+        System.out.println(x + y); // 13
+        System.out.println(x - y); // 7
+        System.out.println(x * y); // 30
+        System.out.println(x / y); // 3
+        System.out.println(x % y); // 1
+
+        System.out.println("Post and pre increment");
+        int z = 5;
+        ++z;
+        System.out.println(z); // 6
+        --z;
+        System.out.println(z); // 5
+
+        System.out.println(z++);
+        z--;
+        System.out.println(z);
+
+    }
+}
